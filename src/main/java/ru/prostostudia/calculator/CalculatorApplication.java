3fac212dddddaa2f2d1d39fd753331a3d6b67ec6
Roman Kuzmin <ghelatin@mail.ru>
2024-09-17 21:32:04 +0300

    commit init

---
+++ src/main/java/ru/prostostudia/calculator/CalculatorApplication.java
@@ -0,0 +1,14 @@
+package ru.prostostudia.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
